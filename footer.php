c41ced9a1f9176497061f6e443af121752d34590
meet-ke <meet.darji@kernelequity.com>
2024-12-24 10:34:39 +0530

    Update File

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>   
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 
+
+    <!-- <script>
+        swal("Good job!", "You clicked the button!", "success");
+    </script> -->
 </head>
 </body>
 </html> 
